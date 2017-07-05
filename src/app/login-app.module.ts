9d9ff4e42c97a26a2f67c3b3f6658504a3b9cfaa
nitu singh <nitu.sin122@gmail.com>
2017-07-05 13:59:16 +0530

    Ang2-S5 Assign5.3 completed

--- src/app/login-app.module.ts
+++ src/app/login-app.module.ts
@@ -1,12 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-import { AppComponent } from './app.component';
 import { LoginComponent } from './Login/login.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
     LoginComponent
   ],
   imports: [
